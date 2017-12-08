e70462399b85f30c113c9e9feac0d50e695acce0
seifhussam <Seifel-din.mostafa@student.guc.edu.eg>
2017-12-08 05:41:50 +0200

    Add files via upload

---
+++ SayeS.ino
@@ -0,0 +1,182 @@
+
+#include <SoftwareSerial.h>
+
+class Color {
+  public : bool red, green, blue;
+    Color (bool r, bool g, bool b) {
+      red =  r;
+      green =  g;
+      blue =  b;
+    }
+};
+Color color_red (true, false, false);
+Color color_green (false, true, false);
+Color color_blue(false, false, true);
+Color color_white(false, false, false);
+
+
+/*
+   status 0 = available    // blinking green
+   status 1 = not availble // blinking red
+   status 2 = pending   // blinking yellow
+*/
+int one_minute = 60000;
+int delay_led = 200;
+int minute = 240;
+class RGBled
+{
+  public:
+    int Red;
+    int Green;
+    int Blue;
+    int redPin;
+    int greenPin;
+    int bluePin;
+    int led_status;
+    int id;
+    int sensorPin;
+    int c ;
+    RGBled(int id1, int redPin1, int greenPin1, int bluePin1, int sensorPin1)
+    {
+      id = id1;
+      Red = color_green.red;
+      Green = color_green.green;
+      Blue = color_green.blue;
+      led_status = 0;
+      c = 0;
+      redPin = redPin1;
+      greenPin = greenPin1;
+      bluePin = bluePin1;
+      sensorPin = sensorPin1;
+      pinMode(sensorPin, INPUT);
+      pinMode(redPin, OUTPUT);
+      pinMode(greenPin, OUTPUT);
+      pinMode(bluePin, OUTPUT);
+    }
+    void start() {
+      
+      turnOn();
+    }
+    void turnOn() {
+      
+      if (!digitalRead(sensorPin)) {
+        setOccupied();
+      } else {
+        switch (led_status) {
+          case 0 :setAvailable() ; break;
+          case 1 : setAvailable() ; break;
+          case 2 : if (c < minute) {
+              c++;
+              setPending();
+            } else {
+              setAvailable();
+              c = 0;
+            }
+            break;
+        }
+      }
+    }
+    void writeColor() {
+      digitalWrite(redPin, Red);
+      digitalWrite(greenPin, Green);
+      digitalWrite(bluePin, Blue);
+    }
+    void setColor(Color c) {
+      Red = c.red;
+      Green = c.green;
+      Blue = c.blue;
+      writeColor();
+    }
+    void setAvailable() {
+      led_status = 0;
+      setColor(color_green); 
+    }
+    void setOccupied() {
+      led_status = 1;
+      setColor(color_red) ; 
+    }
+    void setPending() {
+      led_status = 2;
+      setColor(color_blue);
+    }
+    void cancelPending() {
+      setAvailable();
+    }
+
+
+};
+//#define COMMON_ANODE
+// id ,R ,G ,B, SensorPin
+RGBled ParkingSlot1(1 , 2, 3, 4 , A0);
+RGBled ParkingSlot2(2 , 5, 6, 7, A1);
+RGBled ParkingSlot3(3 , 8, 9, 10 , A2);
+RGBled ParkingSlot4(4 , 11, 12, 13, A4);
+//RGBled green(  0,   255, 0, 0 , 11, 10, 9);
+//RGBled yellow( 100, 200, 0, 0 , 11, 10, 9);
+//Color white(   255,   255,   255, 0 , 9, 10, 11);
+String statusToString() {
+  String x =
+    String("<") + ParkingSlot1.id + String(":") + ParkingSlot1.led_status + String(",") +
+    ParkingSlot2.id + String(":") + ParkingSlot2.led_status + String(",") +
+    ParkingSlot3.id + String(":") + ParkingSlot3.led_status + String(",") +
+    ParkingSlot4.id + String(":") + ParkingSlot4.led_status +
+    String(">");
+  return x;
+}
+void setPendingID(char c) {
+  switch (c) {
+    case '1': ParkingSlot1.setPending() ; break;
+    case '2': ParkingSlot2.setPending() ; break;
+    case '3': ParkingSlot3.setPending(); break;
+    case '4': ParkingSlot4.setPending(); break;
+  }
+}
+void refresh () {
+  ParkingSlot1.start();
+  ParkingSlot2.start();
+  ParkingSlot3.start();
+  ParkingSlot4.start();
+}
+void setup() {
+  //  Serial.begin(115200); // debugg
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  //Serial.println(minute);
+
+}
+String s = "";
+String pending = "";
+bool flag = false ;
+//void loop (){
+////       ParkingSlot1.setColor(color_blue);  ParkingSlot1.writeColor();
+//ParkingSlot4.setAvailable();
+//delay(1000);
+//
+//}
+void loop() { // run over and over
+  // refresh
+  for (int i = 0 ; i <10;i++){
+  refresh(); // 1
+  delay(500);
+  }
+ 
+  
+  String s = statusToString();
+  for (int i = 0 ; i < s.length() ; i++) {
+    Serial.write(s.charAt(i));
+  }
+  pending = "";
+  while (Serial.available()) {
+    char c = Serial.read();
+    pending += c ;
+  }
+  if (!pending.startsWith("f")) {
+    for (int i = 0 ; i < pending.length() ; i += 2) {
+      char c = pending.charAt(i);
+      setPendingID(c);
+    }
+  }
+
+}
---
+++ SayeS_Node.ino
@@ -0,0 +1,173 @@
+/*
+    HTTP over TLS (HTTPS) example sketch
+
+    This example demonstrates how to use
+    WiFiClientSecure class to access HTTPS API.
+    We fetch and display the status of
+    esp8266/Arduino project continuous integration
+    build.
+
+    Created by Ivan Grokhotkov, 2015.
+    This example is in public domain.
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <SoftwareSerial.h>
+
+const char* ssid = "Seif";
+const char* password = "01111227871";
+
+const char* host = "5ee5c7ca.ngrok.io";
+const int httpsPort = 443;
+String url = "/updateSpots";
+
+String is_pending_str = "is_pending" ; 
+String pending_spots_str = "pending_spots";
+String sendUpdates(String data) {
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  //  Serial.print("connecting to ");
+  //  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    //    Serial.println("connection failed");
+    return "";
+  }
+  //  Serial.print("requesting URL: ");
+  //  Serial.println(url);
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n" +
+               "Content-Type: text/html\r\n" +
+               "Content-Length: " + data.length() + "\r\n" +
+               "\r\n" +
+               data + "\n");
+  //  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      //      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  //  Serial.println("reply was:");
+  //  Serial.println("==========");
+  return line ;
+  //  Serial.println("==========");
+  //  Serial.println("closing connection");
+}
+void WifiSetup() {
+  //  Serial.println();
+  //  Serial.print("connecting to ");
+  //  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //    Serial.print(".");
+  }
+   digitalWrite(D0, HIGH);
+  //  Serial.println("");
+  //  Serial.println("WiFi connected");
+  //  Serial.println("IP address: ");
+  //  Serial.println(WiFi.localIP());
+}
+String getBoolean (String s ) {
+  //14 - 19
+
+    return s.substring(14, 19);
+  
+}
+
+
+String getPending(String s) {
+  String res = "" ;
+  for (int index = 36 ; index < s.length() && s[index] != '\"'; index ++ ) {
+    res += s[index];
+  }
+  return res;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(D0, OUTPUT);
+  WifiSetup();
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+bool startreading = false ;
+String arduino_spots = "";
+char c;
+String pending_spots;
+String parsedString;
+int i;
+String flag ;
+void startNode () {
+  if (Serial.available()) {
+    c = Serial.read();
+    if (startreading)
+      arduino_spots += c;
+    if (c == '<')
+      startreading = true ;
+    else if (c == '>') {
+      startreading = false ;
+      pending_spots = sendUpdates(arduino_spots.substring(0, arduino_spots.length() - 1));
+      flag = getBoolean(pending_spots);
+      if (flag.equals("true,")||flag.equals("true")) {
+        parsedString = getPending(pending_spots);
+        for ( i = 0; i < parsedString.length(); i++)
+          Serial.write(parsedString.charAt(i));
+      }
+      else {
+        for ( i = 0; i < flag.length(); i++)
+          Serial.write(flag.charAt(i));
+      }
+      arduino_spots = "";
+    }
+  }
+}
+//String error = "error";
+//String parsed ;
+//int myindex ;
+//int secIndex;
+//int startIndex;
+//int endIndex;
+//String temp;
+//String parseString (String attr,String Json, bool flag){
+////if (attr.length() > Json.length() ) 
+////return error;   
+//
+//for (myindex = 0 ; myindex < Json.length() - attr.length(); myindex ++ ){
+//  temp = Json.substring(myindex,myindex+attr.length()-1);
+//  if (temp.equals(attr)){
+//    // return value
+//    startIndex = -1; 
+//    endIndex = -1 ;
+//    for (secIndex = myindex+attr.length() ;secIndex<Json.length();secIndex++){
+//      if (startIndex>-1){
+//        if (flag)
+//        if (Json[secIndex] == '\"'){
+//          endIndex = secIndex; 
+//          break;
+//        } else if (Json[secIndex] == ','){
+//           endIndex = secIndex; 
+//          break;
+//        }
+//        
+//      }
+//      else {
+//        if(isAlphaNumeric(Json[secIndex])){
+//          startIndex = secIndex;
+//        }
+//      }
+//    }
+//    return Json.substring(startIndex,endIndex);
+//  }
+//}
+//return error;
+//}
+void loop() {
+  startNode();
+}
